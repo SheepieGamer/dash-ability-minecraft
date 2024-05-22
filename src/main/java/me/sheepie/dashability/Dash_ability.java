0385bd145245699cf1f87b9de42ba8623a54eebf
BrownSheepie2021 <86570972+BrownSheepie2021@users.noreply.github.com>
2024-05-22 16:17:59 -0400

    Initial Commit

---
+++ src/main/java/me/sheepie/dashability/Dash_ability.java
@@ -0,0 +1,17 @@
+package me.sheepie.dashability;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dash_ability extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
